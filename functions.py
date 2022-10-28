6deb2ec6b587d317eac7cb66e0f1600b371dacab
Mohet <mgargg2@jaguarlandrover.com>
2022-10-28 11:20:58 +0100

    JIRA-001: unit test added

---
+++ functions.py
@@ -0,0 +1,3 @@
+def create_customer():
+    # TODO: the actual logic
+    return {"name":"Alice"}
---
+++ test_functions.py
@@ -0,0 +1,7 @@
+import functions
+
+def test_create_customer():
+    # assemble
+    # act
+    # assert
+    assert functions.create_customer() == {"name": "Alice"}
